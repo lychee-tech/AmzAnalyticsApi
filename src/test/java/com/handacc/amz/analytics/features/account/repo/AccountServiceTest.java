543d3c794307f0c67f3b99a057399231e90cfb59
Luyuan Nie <Luyuan Nie>
2017-06-05 21:43:55 -0700

    add controller test and service test

--- src/test/java/com/handacc/amz/analytics/features/account/repo/AccountServiceTest.java
+++ src/test/java/com/handacc/amz/analytics/features/account/repo/AccountServiceTest.java
@@ -3,34 +3,35 @@ package com.handacc.amz.analytics.features.account.repo;
 import com.handacc.amz.analytics.features.account.dto.CreateAccountRequest;
 import com.handacc.amz.analytics.features.account.entity.UserEntity;
 import com.handacc.amz.analytics.features.account.service.AccountService;
-import org.apache.catalina.User;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UserRepoTest {
+public class AccountServiceTest {
     @Autowired
-    private UserRepo userRepo;
+    UserRepo userRepo;
 
     @Autowired
-    private AccountService accountService;
+    AccountService accountService;
 
     @Test
-    public void simpleSaveTest(){
-        UserEntity user = new UserEntity();
-        user.setLogin("wlin");
-        user.setFirstName("wenhao");
-        user.setLastName("lin");
-        user.setEmail("wenhao.lin@gmail.com");
-        user.setPassword("P@ssword1");
-        userRepo.save(user);
-
+    public void createAccountTest(){
+        CreateAccountRequest accountRequest = new CreateAccountRequest();
+        accountRequest.setLogin("yaoyuannie");
+        accountRequest.setFirstName("luyuan");
+        accountRequest.setLastName("Nie");
+        accountRequest.setEmail("nie.luyuan@gmail.com");
+        accountRequest.setPassword("123456");
+        accountRequest.setPhone("5103005484");
+        UserEntity user = accountService.createAccount(accountRequest);
         UserEntity saved = userRepo.findOne(user.getId());
         assertNotNull(saved.getId());
         assertEquals(user.getLogin(), saved.getLogin());
